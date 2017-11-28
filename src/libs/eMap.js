dbaf0c56b1e6bc236ba1d6e48ad2552f4e70fb0f
zhe-he <hezhe@ihangmei.com>
2017-11-28 16:35:16 +0800

    end

--- src/libs/eMap.js
+++ src/libs/eMap.js
@@ -17,6 +17,7 @@ class EMap{
     AllData = []
     Data = []
     data = []
+    option = {}
 
     constructor({id,title}){
         this.domBox = document.getElementById(id);
@@ -24,12 +25,15 @@ class EMap{
         this.myChart = echarts.init(this.domBox); 
         this.setList();
         this.setData();
+        this.setOption();
         this.init();
     }
     setList(){
-        this.domList = document.createElement('ul');
+        var style = document.createElement('style');
+        this.domList = document.createElement('div');
+        this.domList.className = 'eMap-list';
         this.domBox.appendChild(this.domList);
-        this.domList.innerHTML = `
+        this.domList.innerHTML = `<ul>
             <li>北京</li>
             <li>上海</li>
             <li>济南</li>
@@ -39,19 +43,43 @@ class EMap{
             <li>郑州</li>
             <li>南昌</li>
             <li>南宁</li>
-            <li>兰州</li>
+            <li>兰州</li></ul>
         `;
-        this.domList.style.cssText = `
-            position: absolute;
-            top: 50%;
-            right: 0;
-            margin-top: -211px;
-            border: 1px solid #f1f1f1;
-            padding: 10px 40px;
-            color: #282828;
-            font-size: 16px;
-            line-height: 40px;
-        `
+        style.innerHTML = `
+            .eMap-list{
+                position: absolute;
+                top: 50%;
+                right: 0;
+                margin-top: -211px;
+                border: 1px solid #f1f1f1;
+                padding: 10px 40px;
+                color: #282828;
+                font-size: 16px;
+                line-height: 40px;
+            }
+            @media only screen and (max-width: 767px){
+                .eMap-list{
+                    position: relative;
+                    top: 0;
+                    margin-top: -50px;
+                    border: 1px solid #fbf1c2;
+                    padding: 4px;
+                }
+                .eMap-list ul{
+                    border: 1px solid #f1f1f1;
+                    overflow: hidden;
+                    padding: 6px 0;
+                }
+                .eMap-list li{
+                    float: left;
+                    width: 20%;
+                    text-align: center;
+                    font-size: 12px;
+                    line-height: 2;
+                }
+            }
+        `;
+        document.head.appendChild(style);
     }
 
     setData(){
@@ -100,9 +128,41 @@ class EMap{
         }
         return res;
     }
-    init(){  
+    init(){
+
+        var max = this.AllData.length;
+        var start = 0;
+        setInterval( ()=>{
+            start += 10;
+            if (start + 20 > max) {
+                start = 0;
+            }
+            this.Data = clone(this.AllData).slice(start,start+20);
+            this.option.series[0].data = this.convertData(this.Data);
+            this.option.series[1].data = this.convertData(this.Data);
+            this.option.series[2].data = this.Data.map(  dataItem => {
+                if(this.geoCoordMap[dataItem[1].name] != undefined){
+                    return {
+                        name: dataItem[1].name,
+                        value: this.geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
+                    };
+                }
+            })
+            this.myChart.setOption(this.option);
+        },5000);
+        this.myChart.setOption(this.option);
+
+        window.addEventListener('resize',()=>{
+            this.setOption();
+            // 此处最多需要5秒更改，如需立即更改，打开158行
+            // this.myChart.setOption(this.option);
+            this.myChart.resize();
+        },false);
+    }
+    setOption(){  
         var color = ['#369cf4', '#ffa022', '#46bee9'];
         var series = [];
+        var isMini = window.innerWidth <= 767; 
         series.push(
             {
                     type: 'lines',
@@ -128,7 +188,7 @@ class EMap{
                 lineStyle: {
                     normal: {
                         color: '#fa8100',
-                        width: 2,
+                        width: isMini?1:2,
                         opacity: 0.9,
                         curveness: 0.2
                     }
@@ -182,16 +242,19 @@ class EMap{
                 }
             }
         );
-        var option = {
+        this.option = {
             title : {
                 text: this.title,
                 textStyle: {
-                    fontSize: 30,
+                    fontSize: isMini?16:30,
                     color: '#475072',
                     fontWeight: 'normal'
                 },
                 left: 'center',
-                top: 40
+                top: isMini?20:40
+            },
+            textStyle:{
+                fontSize: isMini?8:12
             },
             tooltip : {
                 show: true,
@@ -213,32 +276,6 @@ class EMap{
             },
             series: series
         };
-        
-        var max = this.AllData.length;
-        var start = 0;
-        setInterval( ()=>{
-            start += 10;
-            if (start + 20 > max) {
-                start = 0;
-            }
-            this.Data = clone(this.AllData).slice(start,start+20);
-            option.series[0].data = this.convertData(this.Data);
-            option.series[1].data = this.convertData(this.Data);
-            option.series[2].data = this.Data.map(  dataItem => {
-                if(this.geoCoordMap[dataItem[1].name] != undefined){
-                    return {
-                        name: dataItem[1].name,
-                        value: this.geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
-                    };
-                }
-            })
-            this.myChart.setOption(option);
-        },5000);
-        this.myChart.setOption(option);
-
-        window.addEventListener('resize',()=>{
-            this.myChart.resize();
-        },false);
     }
 }
 
--- src/views/about.vue
+++ src/views/about.vue
@@ -9,19 +9,24 @@
                 </div>
             </div>
             <div class="main">
-                
-                <div class="road-map" id="road-map"></div>
-                <div class="road-circle" id="road-circle"></div>
-                
-                <div ref="more" class="more">
-                    <h3>连接<em>160+</em>个城市</h3>
-                    <p>北京、上海、广州、深圳、杭州、成都、南京、青岛、武汉、济南、哈尔滨、<br/>郑州、呼和浩特、厦门、南宁…</p>
-                    <h3>覆盖沿海发达城市<em>90%</em>铁路线路</h3>
-                    <h3>客流量超过<em>18亿</em>人次/年</h3>
-                    <h3><em>500万+</em>人次/日</h3>
-                    <h3><em>1700万+</em>车次/日</h3>
-                    <h3>超过<em>22000</em>节车厢开通了上网服务</h3>
+                <div>
+                    <p class="main-mobile">广州美正在线网络科技有限公司由中国最具影响力的中高端户外媒体运营商之一——航美集团控股，是中国专业运营高铁列车Wi-Fi、普速列车Wi-Fi媒体的互联网传播机构。</p>
+                    <div class="road-map" id="road-map"></div>
+                    <div class="road-circle" id="road-circle"></div>
                 </div>
+            </div>
+             
+            <div ref="more" class="more media_more">
+                <h3>连接<em>160+</em>个城市</h3>
+                <div>
+                    <p>北京、上海、广州、深圳、杭州、成都、南京、青岛、武汉、济南、哈尔滨、<br/>郑州、呼和浩特、厦门、南宁…</p>
+                    <p>北京、上海、广州、深圳、杭州、成都、南京、青岛、<br/>武汉、济南、哈尔滨、郑州、呼和浩特、厦门、南宁…</p>
+                </div>
+                <h3>覆盖沿海发达城市<em>90%</em>铁路线路</h3>
+                <h3>客流量超过<em>18亿</em>人次/年</h3>
+                <h3><em>500万+</em>人次/日</h3>
+                <h3><em>1700万+</em>车次/日</h3>
+                <h3>超过<em>22000</em>节车厢开通了上网服务</h3>
             </div>
             
             <div class="end">
@@ -39,9 +44,12 @@
     
     export default {
         data(){
-            return {}
+            return {
+                option: {}
+            }
         },
         mounted(){
+            this.getOption();
             this.$nextTick(()=>{
                 this.drawCircle();
                 new EMap({id:'road-map',title:'美正普速WiFi覆盖全国十大路局'});
@@ -51,31 +59,55 @@
         methods: {
             drawCircle(){
                 var circle = new echarts.init(document.getElementById("road-circle"));
-                var option = {
-                    title: {
-                        text: '覆盖率',
-                        top: "39%",
-                        right: 5,
-                        textStyle: {
-                            color: '#282828',
-                            fontWeight: 'normal',
-                            fontSize: 18,
-                            textShadowOffsetY: 10
-                        }
-                    },
+                circle.setOption(this.option);
+                window.addEventListener('resize',()=>{
+                    this.getOption();
+                    circle.setOption(this.option);
+                    circle.resize();
+                },false);
+            },
+            getOption(){
+                var isMini = window.innerWidth <= 767;
+                var legend = {
+                    top: 'middle',
+                    left: 'right',
+                    data: ['美正','其他'],
+                    orient: 'vertical'
+                };
+                var title = {
+                    text: '覆盖率',
+                    top: "39%",
+                    right: 5,
+                    textStyle: {
+                        color: '#282828',
+                        fontWeight: 'normal',
+                        fontSize: 18,
+                        textShadowOffsetY: 10
+                    }
+                }
+                if (isMini) {
+                    title.text += ':';
+                    title.bottom = 20;
+                    title.left = "20%";
+                    delete title.top;
+                    delete title.right;
+                    title.textStyle.fontSize = 16;
+
+                    legend.bottom = 20;
+                    legend.left = "40%";
+                    legend.orient = 'horizontal';
+                    delete legend.top;
+                }
+                this.option = {
+                    title,
                     tooltip : {
                         trigger: 'item',
                         formatter: '{b}: {c}%'
                     },
                     textStyle: {
-                        fontSize: 16
+                        fontSize: isMini?12:16
                     },
-                    legend: {
-                        top: 'middle',
-                        left: 'right',
-                        data: ['美正','其他'],
-                        orient: 'vertical'
-                    },
+                    legend,
                     color: ['#fc6719','#fec500'],
                     series : [
                         {
@@ -101,10 +133,6 @@
                         }
                     ]
                 };
-                circle.setOption(option);
-                window.addEventListener('resize',()=>{
-                    circle.resize();
-                },false);
             },
             scroll(){
                 var _this = this;
@@ -114,7 +142,7 @@
                     var h = _this.$refs.more.offsetHeight;
                     var H = window.innerHeight;
                     if(t < Math.min(h,H)){
-                        _this.$refs.more.className = 'more animation';
+                        _this.$refs.more.className = 'more media_more animation';
                         window.removeEventListener('scroll',fn);
                     }
                 }
