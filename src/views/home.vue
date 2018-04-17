2e44c63f5d6a519a602e5c5a6c4da1f4528741bc
sunhongkui <sunhongkui@ihangmei.com>
2018-04-17 09:42:21 +0800

    add sm modal

--- src/views/home.vue
+++ src/views/home.vue
@@ -106,6 +106,23 @@
             </div>
         </div>
         <mz-foot></mz-foot>
+         <!-- 声明 -->
+         <div class="select-overlay" v-show="isShowSm">
+          <div class="content">
+            <a href="javascript:;" class="close-sm" @click="isShowSm = false">关闭 X</a>
+            <h3><span>严正声明</span></h3>
+            <p>
+                <span class="orange">“往返WiFi”</span>业务为航美在线网络科技集团有限公司旗下广州美正在线网络科技有限公司（以下简称“我司”）所有，其知识产权及相关权利均属于我司。关于近期市场上出现的销售铁路局往返WiFi资源的情况，我公司严正声明如下：<br />
+                1. 截止目前，我司未针对铁路<span class="orange">“往返WiFi”十个铁路局的WIFI运营权</span>进行对外授权，我司以外的任何主体实施的销售该资源的行为皆不具有合法授权；由此造成的他人财产损失及权益损害，我司不承担任何责任。<br />
+                2. 不具有代理资格、非法销售我司资源的行为已严重损害了我司的合法权益，我司将根据保护自身权益的需求，保留随时诉诸法律的权利。
+            </p>
+            <br />
+            <p style="text-align: right;">
+               广州美正在线网络科技有限公司<br />
+               2018年4月12日
+            </p>
+          </div>
+         </div> 
     </div>
 </template>
 
@@ -117,7 +134,8 @@
                 advantage,
                 group,
                 cooperation,
-                connect
+                connect,
+                isShowSm:true
             }
         },
         methods: {
